981d14dc999e3b41888d2536aaa624291c5b2d4f
Codesofnati <nathnaelasefa80@gmail.com>
2026-02-26 23:25:28 +0300

    fixed

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -342,7 +342,7 @@ export default function AdminPageContent() {
         </header>
 
         {/* Dynamic Content Area */}
-        <div className="p-6">
+        <div className="">
           <AnimatePresence mode="wait">
             <motion.div
               key={section}
